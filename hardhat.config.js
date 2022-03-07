6d3e6444f6d8eb3462dc2ef34d655523d86ff601
Denis Potapov <denipotapo@yandex.ru>
2022-03-07 23:34:45 +0300

    fix config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+dotenv.config();
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -10,12 +11,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: 'rinkeby',
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts: [
+        process.env.PRIVATE_KEY!,
+        process.env.CONTRIBUTOR_PRIVATE_KEY!,
+      ],
+    },
+  },
 };
