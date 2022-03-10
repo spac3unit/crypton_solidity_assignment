baf6a63117c26cc09603699111dbf437d1504f13
Denis Potapov <denipotapo@yandex.ru>
2022-03-10 15:31:18 +0300

    add tasks

---
+++ tasks/index.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-web3");
+require('dotenv').config();
+
+const {abi} = require('../artifacts/contracts/Donations.sol/Donations');
+const Web3 = require('web3');
+const web3 = new Web3 (new Web3.providers.HttpProvider(process.env.PROVIDER_URL));
+const DonationContract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+
+// CONTRACT_ADDRESS: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+task("donate", "send funds to Donations contract")
+    .addParam("account", "")
+    .addParam("amount", "")
+    .setAction(async (taskArgs) => {
+        const amount = web3.utils.toWei(taskArgs.amount);
+        await DonationContract.methods.makeDonation().send({from:taskArgs.account, value:amount})
+            .on('receipt', function(receipt) {
+                console.log(receipt)
+            })
+        });
+
+task("getContractBalance", "Prints an contract's balance")
+	.setAction(async () => {
+		await DonationContract.methods.getContractBalance().call().then(console.log)
+			
+	});
+
+task("getDonatersList", "Prints all donators addresses who donated to contract")
+    .setAction(async () => {
+    	await DonationContract.methods.getDonatersList()
+			.call()
+			.then(result => console.log(result));
+    });
+
+module.exports = {};
